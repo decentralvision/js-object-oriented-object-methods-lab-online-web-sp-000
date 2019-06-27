85e740b6e15fa080aa530eca6bb860962c2ec1f8
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-27 18:23:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,25 +2,27 @@
 
 const BoardMember = function(name, homeState, training) {
   [this.name, this.homeState, this.training] = [name, homeState, training]
+
+  this.veto = function(){
+    return "No, I must disagree"
+  }
+  // + `approve` — returns `You can do that!`
+  this.approve = function(){
+    return "You can do that!"
+  }
+  // + `doCharity` — returns `I like to help people.`
+  this.doCharity = function(){
+    return "I like to help people."
+  }
+  // + `releasePressStatement` — returns `You will see great things from Scuber.`
+  this.releasePressStatement = function(){
+    return "You will see great things from Scuber."
+  }
+  // + `sayHi` — returns `"Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.`
+  this.sayHi = function(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
+
 }
 // Add following methods to the class:
 // + `veto` — returns `No, I must disagree`
-BoardMember.veto = function(){
-  return "No, I must disagree"
-}
-// + `approve` — returns `You can do that!`
-BoardMember.approve = function(){
-  return "You can do that!"
-}
-// + `doCharity` — returns `I like to help people.`
-BoardMember.doCharity = function(){
-  return "I like to help people."
-}
-// + `releasePressStatement` — returns `You will see great things from Scuber.`
-BoardMember.releasePressStatement = function(){
-  return "You will see great things from Scuber."
-}
-// + `sayHi` — returns `"Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.`
-BoardMember.sayHi = function(){
-  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-}
